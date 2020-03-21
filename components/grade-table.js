7f8991ab5e5352e6c43e8018fcaabcead809c599
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-03-20 17:28:12 -0700

    made for loop in grade-table.js to loop through student array

--- components/grade-table.js
+++ components/grade-table.js
@@ -5,6 +5,21 @@ class GradeTable{
   }
   updateGrades(grades){
     // updateGrades takes one parameter, grades and logs it to the console for the time being.
-    console.log(grades)
+    this.tableElement.querySelector("tbody").innerHTML = "";
+    for (var arrayCounter = 0; arrayCounter < grades.length; arrayCounter++) {
+      var tableData1 = document.createElement("td")
+      tableData1.textContent = grades[arrayCounter].name
+      var tableData2 = document.createElement("td")
+      tableData2.textContent = grades[arrayCounter].course
+      var tableData3 = document.createElement("td")
+      tableData3.textContent = grades[arrayCounter].grade
+      var tableRow = document.createElement("tr")
+      tableRow.appendChild(tableData1)
+      tableRow.appendChild(tableData2)
+      tableRow.appendChild(tableData3)
+      tBody.appendChild(tableRow)
+    }
+    var data1 = document.createElement("td")
+    data1.textContent =
   }
 }
